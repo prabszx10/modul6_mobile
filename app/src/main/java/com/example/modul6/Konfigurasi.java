4890958379c615265c866990e9730a74512716dc
prabszx10 <prabszx10@gmail.com>
2020-05-15 06:04:23 +0700

    Dicky Prabowo 201810370311123

---
+++ app/src/main/java/com/example/modul6/Konfigurasi.java
@@ -0,0 +1,25 @@
+package com.example.modul6;
+
+public class Konfigurasi {
+
+    public static final String URL_ADD="https://dickyprabowo321.000webhostapp.com/MOBILE/insert.php";
+    public static final String URL_GET_ALL ="https://dickyprabowo321.000webhostapp.com/MOBILE/read.php";
+    public static final String URL_GET_MHS ="https://dickyprabowo321.000webhostapp.com/MOBILE/select.php?id=";
+    public static final String URL_UPDATE_MHS ="https://dickyprabowo321.000webhostapp.com/MOBILE/update.php";
+    public static final String URL_DELETE_MHS ="https://dickyprabowo321.000webhostapp.com/MOBILE/delete.php?id=";
+
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+    //ID Mahasiswa
+//mhs itu singkatan dari Mahasiswa
+    public static final String MHS_ID = "mhs_id";
+}
